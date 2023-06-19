60d83f38180062b191e0a6c1c785c545b58849fe
ASmith-eng <alexsmith194@outlook.com>
2023-06-19 22:25:36 +0100

    Add react routes + register/login pages

---
+++ public/src/pages/Chat.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Chat() {
+    return (
+        <div>
+            <div>Chat</div>
+            <footer>
+                <span>
+                    <a href="https://www.flaticon.com/free-animated-icons/ocean" title="ocean animated icons">Ocean animated icons created by Freepik - Flaticon</a>
+                </span>
+            </footer>
+        </div>
+    )
+}
+
+export default chat
