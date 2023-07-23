6cb8ef19c5b45ed7c373536b4fb527bedf2fe450
ASmith-eng <alexsmith194@outlook.com>
2023-07-23 22:30:52 +0100

    Add register back end functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,32 @@
-module.exports.register = (req, res, next) => {
-    console.log("in controller");
-    console.log(req.body);
-};
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+
+module.exports.register = async (req, res, next) => {
+    try {
+        const { username, email, password } = req.body;
+
+        const usernameCheck = await User.findOne({ username });
+        if(usernameCheck) {
+            return res.json({ msg: "This username is already taken", status: false });
+        }
+
+        const emailCheck = await User.findOne({ email });
+        if(emailCheck) {
+            return res.json({ msg: "This email is already registered", status: false });
+        }
+
+        const encryptedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            email: email,
+            username: username,
+            password: encryptedPassword
+        });
+        delete user.password;
+
+        return res.json({ status: true, user });
+    }
+    catch (err) {
+        next(err);
+    }
+}
