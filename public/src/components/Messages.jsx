fc9d9403ac6c25e2a6d7b4583b3095bb72f60756
ASmith-eng <alexsmith194@outlook.com>
2023-08-25 13:11:36 +0100

    feat: Show message history in chat

--- public/src/components/Messages.jsx
+++ public/src/components/Messages.jsx
@@ -1,12 +1,59 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export default function Messages() {
+export default function Messages({conversation}) {
     return (
-        <Container>Messages</Container>
+        <>
+            <Container>
+                {
+                    conversation.map((message) => {
+                        return (
+                            <div>
+                                <div className={`message ${message.fromSelf ? "sent" : "received"}`}>
+                                    <div className="content">
+                                        <p>
+                                            {message.message}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </Container>
+        </>
     )
 }
 
 const Container = styled.div`
     height: 80%;
+    padding: 1rem 2rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    overflow: auto;
+    .message {
+        display: flex;
+        align-items: center;
+        .content {
+            max-width: 45%;
+            overflow-wrap: break-word;
+            padding: 0.5rem 1rem;
+            font-size: 1.1rem;
+            border-radius: 0.6rem;
+            color: white;
+        }
+    }
+    .sent {
+        justify-content: flex-end;
+        .content {
+            background-color: #00CDCD;
+        }
+    }
+    .received {
+        justify-content: flex-start;
+        .content {
+            background-color: #FFFFFF39;
+        }
+    }
 `;
